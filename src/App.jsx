e92a9680cf2963332bea83dccb78662b67d3036a
pehesara112455 <upehesara827@gmail.com>
2025-11-25 20:45:56 +0530

    Setup environment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world! This should be big, bold, blue, and underlined.
+      </h1>
+    </div>
+  )
+}
+
+export default App
